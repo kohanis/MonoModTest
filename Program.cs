cd95a9ce5bd95d8f0553cb609ce820c1962de8e2
kohanis <kohanis@ya.ru>
2023-12-22 15:52:35 +0300

    Not an issue. Mono problem?

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using MonoMod.Core.Platforms;
 using MonoMod.RuntimeDetour;
 using MonoMod.Utils;
 
@@ -9,10 +13,12 @@ namespace MonoModTest
         public static void Main(string[] args)
         {
             Test();
+            MonoTest();
         }
 
         private static void Test()
         {
+            
             unsafe {
                 var clib = PlatformDetection.OS switch {
                     OSKind.Windows => "msvcrt",
@@ -36,13 +42,37 @@ namespace MonoModTest
         {
             return 1;
         }
-
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
         private static int EvenMoreFakeRandom()
         {
             return 0;
         }
 
         private delegate int msvcrandDelegate();
+
+        private static void MonoTest()
+        {
+            if(Type.GetType ("Mono.Runtime") is null)
+                return;
+            
+            var original = Assembly.GetExecutingAssembly();
+
+            var getAssemblyMethod = typeof(Assembly).GetMethod(nameof(Assembly.GetExecutingAssembly), new Type[] { });
+            using var getAssemblyHookNative = new NativeHook(PlatformTriple.Current.GetNativeMethodBody(getAssemblyMethod), GetAssemblyFix);
+
+            var patched = Assembly.GetExecutingAssembly();
+            
+            Helpers.Assert(Equals(original, patched));
+        }
+        
+        delegate Assembly GetAssemblyDelegate();
+        
+        private static Assembly GetAssemblyFix(GetAssemblyDelegate orig)
+        {
+            // return orig();
+            return Assembly.GetAssembly(typeof(Program));
+        }
     }
     
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Does not work at all on net35 \
-Second assert fails on any mono and release net48 \
+~~Second assert fails on any mono and release net48~~ \
 net6.0 is fine
