96e7382a45c8a643322ac8ef2d2f606e98813de8
kohanis <kohanis@ya.ru>
2023-12-22 12:10:24 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+
+namespace MonoModTest
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test();
+        }
+
+        private static void Test()
+        {
+            unsafe {
+                var clib = PlatformDetection.OS switch {
+                    OSKind.Windows => "msvcrt",
+                    _ => "c"
+                };
+                
+                var msvcrt = DynDll.OpenLibrary(clib);
+                var msvcrand = (delegate* unmanaged[Cdecl]<int>) DynDll.GetExport(msvcrt, "rand");
+        
+                using (new NativeHook((IntPtr) msvcrand, FakeRandom)) {
+                    Helpers.Assert(msvcrand() == 1);
+                }
+                
+                using (new Hook(() => EvenMoreFakeRandom(), FakeRandom)) {
+                    Helpers.Assert(EvenMoreFakeRandom() == 1);
+                }
+            }
+        }
+
+        private static int FakeRandom(msvcrandDelegate orig)
+        {
+            return 1;
+        }
+
+        private static int EvenMoreFakeRandom()
+        {
+            return 0;
+        }
+
+        private delegate int msvcrandDelegate();
+    }
+    
+
+}
